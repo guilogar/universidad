7356f0cef608672e96455254919e82fb15dd0941
guilogar <jdkdejava@gmail.com>
2017-10-27 20:12:45 +0200

    Practica 2 terminada de aaed. Falta perguntar al profesor.

--- aaed/practicas/segunda/aleat_in.cpp
+++ aaed/practicas/segunda/aleat_in.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
+#include <algorithm>
 
 using namespace std;
 
@@ -14,7 +16,7 @@ double num_aleat_double(double a, double b)
     return (a + ((double) rand() / RAND_MAX) * (b - a));
 }
 
-int dado_aleatorio()
+double dado_aleatorio()
 {
     int v[] = {0, 0, 0, 0, 0, 0};
     
@@ -32,26 +34,62 @@ int dado_aleatorio()
     }
 }
 
+double num_aleat_muy_grande(long n) {
+    double y = 0;
+    
+    for (int i = 0; i < n; i++) {
+        y += 4 * sqrt(1 - pow(num_aleat_double(0, 1), 2));
+    }
+    return (double) y / n;
+}
+
+double perm_pseudo_aleat(int num_natu, long n) {
+    double perm_ord = 0;
+    int numeros[num_natu];
+    
+    for (int i = 1; i <= num_natu; i++) {
+        numeros[i - 1] = i;
+    }
+
+    for (long i = 1; i <= n; i++) {
+        random_shuffle(numeros, numeros + num_natu);
+        
+        if(!next_permutation(numeros, numeros + num_natu)) {
+            perm_ord++;
+        }
+    }
+    return perm_ord;
+}
+
 int main(int argc, const char *argv[])
 {
     int a, b;
     srand(time(0));
     
-    cout << "Dime el número a => ";
-    cin >> a;
-    cout << "Dime el número b => ";
-    cin >> b;
+    /*
+     *cout << "Dime el número a => ";
+     *cin >> a;
+     *cout << "Dime el número b => ";
+     *cin >> b;
+     *
+     *cout << "El número aleatorio discreto es => " << num_aleat_int(a, b) << "." << endl;
+     *
+     *dado_aleatorio();
+     *
+     *cout << "Dime el número a continuo => ";
+     *cin >> a;
+     *cout << "Dime el número b continuo => ";
+     *cin >> b;
+     *
+     *cout << "El número aleatorio continuo es => " << num_aleat_double(a, b) << "." << endl;
+     */
     
-    cout << "El número aleatorio discreto es => " << num_aleat_int(a, b) << "." << endl;
+    /*
+     *double res = num_aleat_muy_grande(10000000L);
+     *std::cout << "Y la media de tantos numeros es => " << res << std::endl;
+     */
     
-    dado_aleatorio();
-    
-    cout << "Dime el número a continuo => ";
-    cin >> a;
-    cout << "Dime el número b continuo => ";
-    cin >> b;
-    
-    cout << "El número aleatorio continuo es => " << num_aleat_double(a, b) << "." << endl;
-    
+    double resu = perm_pseudo_aleat(6, 10000000L);
+    std::cout << "Existen " << resu << " de permutaciones ordenadas." << std::endl;
     return 0;
 }

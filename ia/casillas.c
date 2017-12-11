f451f2f89bf280cb93c85e27939dff3226d8eeaf
guilogar <jdkdejava@gmail.com>
2017-12-11 19:54:02 +0100

    Casillas.c corregido.

--- ia/casillas.c
+++ ia/casillas.c
@@ -4,45 +4,22 @@
 
 #include "casillas.h"
 
-int i = 0, j = 0, coste_total = 0;
+int coste_total = 0;
 
-int obtener_i(tEstado *estado) {
-    int i = 0, j = 0;
-    
-    for (i = 0; i < Ncol; i++) {
-        for (j = 0; j < Nfil; j++) {
-            if(estado->celdas[i][j] == 1) {
-                return i;
-            }
-        }
-    }
-    return i;
-}
-
-int obtener_j(tEstado *estado) {
-    int i = 0, j = 0;
-    
-    for (i = 0; i < Ncol; i++) {
-        for (j = 0; j < Nfil; j++) {
-            if(estado->celdas[i][j] == 1) {
-                return j;
-            }
-        }
-    }
-    return j;
-}
-
-tEstado *crearEstado(int celdas[Ncol][Nfil])
+tEstado *crearEstado(int celdas[Nfil][Ncol])
 {
     tEstado *estado = (tEstado *) malloc(sizeof(tEstado));
     int i, j, ficha;
 
-    for (i=0;i<Ncol;i++) {
-        for (j=0;j<Nfil;j++) {
-           ficha = celdas[i][j];
-           estado->celdas[i][j] = ficha;
-           /*estado->fila[ficha]=i;*/
-           /*estado->col[ficha]=j;*/
+    for (i = 0; i < Nfil; i++) {
+        for (j = 0; j < Ncol; j++) {
+            ficha = celdas[i][j];
+            estado->celdas[i][j] = ficha;
+           
+            if(ficha == 1) {
+                estado->fila = i;
+                estado->columna = j;
+            }
         }
     }
     
@@ -63,8 +40,8 @@ tEstado *estadoObjetivo()
 
 int coste(unsigned op, tEstado *estado)
 {
-    int i = obtener_i(estado);
-    int j = obtener_j(estado);
+    int i = estado->fila;
+    int j = estado->columna;
     int coste = 0;
     switch (op) {
         case ARRIBA:
@@ -86,54 +63,47 @@ int coste(unsigned op, tEstado *estado)
     return coste;
 }
 
-/* VISUALIZACIÓN DE ESTADOS Y OPERADORES*/
-
-
 void dispEstado(tEstado *estado)
 {
     int i, j, k;
-    for (i = 0; i < Ncol; i++) {
+    for (i = 0; i < Nfil; i++) {
         if(i % 2 == 0) {
             printf(ANSI_COLOR_GREEN);
             printf("|   ");
             printf(ANSI_COLOR_RESET);
             printf("%d   ", estado->celdas[i][0]);
-            for(j = 1; j < Nfil; j += 2) {
+            
+            for(j = 1; j < Ncol; j += 2) {
                 printf(ANSI_COLOR_GREEN);
                 int res = estado->celdas[i][j] / 100;
-                if(res == 1) {
-                    printf("|   ");
-                }
-                if(res == 2) {
-                    printf("||  ");
-                }
-                if(res == 3) {
-                    printf("||| ");
-                }
+                
+                if(res == 1) { printf("|   "); }
+                if(res == 2) { printf("||  "); }
+                if(res == 3) { printf("||| "); }
+                
                 printf(ANSI_COLOR_RESET);
                 printf("%d   ", estado->celdas[i][j+1]);
             }
+            
             printf(ANSI_COLOR_GREEN);
             printf("|");
             printf(ANSI_COLOR_RESET);
             puts("");
-            /*printf("i => %d, j => %d.\n", i, j);*/
         } else {
             int mayor_k = 0;
-            for(j = 0; j < Nfil; j += 2) {
+            for(j = 0; j < Ncol; j += 2) {
                 int res = estado->celdas[i][j] / 100;
                 if (res > mayor_k)
                     mayor_k = res;
             }
             
             for (k = 1; k <= mayor_k; k++) {
-                for(j = 0; j < Nfil; j += 2) {
+                for(j = 0; j < Ncol; j += 2) {
                     printf(ANSI_COLOR_GREEN);
                     int res = estado->celdas[i][j] / 100;
-                    if(res - k >= 0)
-                        printf("--------");
-                    else
-                        printf("        ");
+                    
+                    if(res - k >= 0) { printf("--------"); }
+                    else             { printf("        "); }
                 }
                 puts("");
             }
@@ -146,34 +116,30 @@ void dispOperador(unsigned op)
 {
    switch(op)
    {
-      case ARRIBA:    printf("Movimiento ARRIBA:\n"); break;
-      case ABAJO:     printf("Movimiento ABAJO:\n"); break;
-      case IZQUIERDA: printf("Movimiento IZQUIERDA:\n"); break;
-      case DERECHA:   printf("Movimiento DERECHA:\n"); break;
+      case ARRIBA:    printf("Movimiento ARRIBA: \n"); break;
+      case ABAJO:     printf("Movimiento ABAJO: \n"); break;
+      case IZQUIERDA: printf("Movimiento IZQUIERDA: \n"); break;
+      case DERECHA:   printf("Movimiento DERECHA: \n"); break;
    }
 }
 
-// FUNCIONES QUE SE HAN DE IMPLEMENTAR EN LA PRACTICA 1
-
-// Función auxiliar para comprobar si dos puzles tienen las fichas colocadas en el mismo orden en el tablero
-int iguales(tEstado *s, tEstado *t)  //
+int iguales(tEstado *s, tEstado *t)
 {
-    int i, j = Nfil;
+    int i, j, sonIguales = 1;
     
-    for (i = 0; i < Ncol; i++) {
-        for (j = 0; j < Nfil; j++) {
+    for (i = 0; i < Nfil && sonIguales; i++) {
+        for (j = 0; j < Ncol && sonIguales; j++) {
             if(s->celdas[i][j] != t->celdas[i][j]) {
-                return 0;
+                return sonIguales;
             }
         }
     }
-    return 1;
+    return sonIguales;
 }
 
 
 int testObjetivo(tEstado *estado)
 {
-    
     return iguales(estado, estadoObjetivo());
 }
 
@@ -181,10 +147,8 @@ int testObjetivo(tEstado *estado)
 int esValido(unsigned op, tEstado *estado)
 {
     int esValido = 0;
-    int i = obtener_i(estado);
-    int j = obtener_j(estado);
-    i++;
-    j++;
+    int i = estado->fila;
+    int j = estado->columna;
     switch (op) {
         case ARRIBA:
             if(i > 0) {
@@ -193,7 +157,7 @@ int esValido(unsigned op, tEstado *estado)
             }
             break;
         case ABAJO:
-            if(i < Ncol) {
+            if(i < Nfil - 1) {
                 puts("Se puede mover abajo.");
                 esValido = 1;
             }
@@ -205,13 +169,20 @@ int esValido(unsigned op, tEstado *estado)
             }
             break;
         case DERECHA:
-            if(j < Nfil) {
+            if(j < Ncol - 1) {
                 puts("Se puede mover a la derecha.");
                 esValido = 1;
             }
             break;
         default:
             puts("Operador Invalido.");
+    }
+    if(!esValido) {
+        puts("");
+        puts("-----------------------------------");
+        puts("No se puede mover en esa direccion.");
+        puts("-----------------------------------");
+        puts("");
     }
     return esValido;
 }
@@ -221,41 +192,76 @@ tEstado *aplicaOperador(unsigned op, tEstado *estado)
     tEstado *nuevo = (tEstado *) malloc(sizeof(tEstado));
     memcpy(nuevo, estado, sizeof(tEstado));  // Hace una copia del estado
     
+    printf("El coste en esta operacion es => %d.\n", coste(op, nuevo));
+    printf("El coste total es => %d.\n", coste_total);
+    
     int aux;
-    if(esValido(op, nuevo)) {
-        printf("El coste en esta operacion es => %d.\n", coste(op, nuevo));
-        printf("El coste total es => %d.\n", coste_total);
-        
-        switch (op) {
-            case ARRIBA:
-                nuevo->celdas[i][j] = nuevo->celdas[i-2][j];
-                i-=2;
-                nuevo->celdas[i][j] = 1;
-                break;
-            case ABAJO:
-                nuevo->celdas[i][j] = nuevo->celdas[i+2][j];
-                i+=2;
-                nuevo->celdas[i][j] = 1;
-                break;
-            case IZQUIERDA:
-                nuevo->celdas[i][j] = nuevo->celdas[i][j-2];
-                j-=2;
-                nuevo->celdas[i][j] = 1;
-                break;
-            case DERECHA:
-                nuevo->celdas[i][j] = nuevo->celdas[i][j+2];
-                j+=2;
-                nuevo->celdas[i][j] = 1;
-                break;
-            default:
-                puts("por defecto.");
-        }
-    } else {
-        puts("Error, no se puede mover hacia este lado.");
+    int i = estado->fila;
+    int j = estado->columna;
+    switch (op) {
+        case ARRIBA:
+            nuevo->celdas[i][j] = nuevo->celdas[i-2][j];
+            i-=2;
+            nuevo->celdas[i][j] = 1;
+            break;
+        case ABAJO:
+            nuevo->celdas[i][j] = nuevo->celdas[i+2][j];
+            i+=2;
+            nuevo->celdas[i][j] = 1;
+            break;
+        case IZQUIERDA:
+            nuevo->celdas[i][j] = nuevo->celdas[i][j-2];
+            j-=2;
+            nuevo->celdas[i][j] = 1;
+            break;
+        case DERECHA:
+            nuevo->celdas[i][j] = nuevo->celdas[i][j+2];
+            j+=2;
+            nuevo->celdas[i][j] = 1;
+            break;
+        default:
+            puts("por defecto.");
     }
+    nuevo->fila = i;
+    nuevo->columna = j;
     
     dispEstado(nuevo);
     return nuevo;
+    /*
+     *if(esValido(op, nuevo)) {
+     *    printf("El coste en esta operacion es => %d.\n", coste(op, nuevo));
+     *    printf("El coste total es => %d.\n", coste_total);
+     *    
+     *    switch (op) {
+     *        case ARRIBA:
+     *            nuevo->celdas[i][j] = nuevo->celdas[i-2][j];
+     *            i-=2;
+     *            nuevo->celdas[i][j] = 1;
+     *            break;
+     *        case ABAJO:
+     *            nuevo->celdas[i][j] = nuevo->celdas[i+2][j];
+     *            i+=2;
+     *            nuevo->celdas[i][j] = 1;
+     *            break;
+     *        case IZQUIERDA:
+     *            nuevo->celdas[i][j] = nuevo->celdas[i][j-2];
+     *            j-=2;
+     *            nuevo->celdas[i][j] = 1;
+     *            break;
+     *        case DERECHA:
+     *            nuevo->celdas[i][j] = nuevo->celdas[i][j+2];
+     *            j+=2;
+     *            nuevo->celdas[i][j] = 1;
+     *            break;
+     *        default:
+     *            puts("por defecto.");
+     *    }
+     *    nuevo->fila = i;
+     *    nuevo->columna = j;
+     *} else {
+     *    puts("Error, no se puede mover hacia este lado.");
+     *}
+     */
 }
 
 int main(void) {
@@ -263,14 +269,25 @@ int main(void) {
     /*iguales(estadoInicial(), estadoObjetivo());*/
     
     tEstado *estado = estadoInicial();
-    
     dispEstado(estado);
-    estado = aplicaOperador(2, estado);
-    estado = aplicaOperador(4, estado);
-    estado = aplicaOperador(4, estado);
-    estado = aplicaOperador(4, estado);
-    estado = aplicaOperador(2, estado);
-    estado = aplicaOperador(2, estado);
+    
+    int i, op = 2;
+    
+    if(esValido(op, estado)) {
+        estado = aplicaOperador(op, estado);
+    }
+    op = 4;
+    for(i = 0; i < 3; i++) {
+        if(esValido(op, estado)) {
+            estado = aplicaOperador(op, estado);
+        }
+    }
+    op = 2;
+    for(i = 0; i < 3; i++) {
+        if(esValido(op, estado)) {
+            estado = aplicaOperador(op, estado);
+        }
+    }
     
     return 0;
 }
--- ia/casillas.h
+++ ia/casillas.h
@@ -1,10 +1,13 @@
+#ifndef CASILLAS_H
+#define CASILLAS_H
+
 #define ARRIBA 1
 #define ABAJO 2
 #define IZQUIERDA 3
 #define DERECHA 4
 #define NUM_OPERADORES 4
-#define Ncol 7
-#define Nfil 5
+#define Nfil 7
+#define Ncol 5
 
 //===================================
 #define ANSI_COLOR_RED     "\x1b[31m"
@@ -14,16 +17,18 @@
 #define ANSI_COLOR_MAGENTA "\x1b[35m"
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
+//===================================
 
 #ifndef _tEstado_
 #define _tEstado_
    typedef struct {
-        int celdas[Ncol][Nfil];
+        int celdas[Nfil][Ncol];
+        int fila, columna;
    } tEstado;
 #endif
 
 
-static int caja_inicial[Ncol][Nfil]=
+static int caja_inicial[Nfil][Ncol]=
 {
   {1, 100, 0, 100, 0},
   {100, 0, 100, 0, 100},
@@ -35,7 +40,7 @@ static int caja_inicial[Ncol][Nfil]=
 };
 
 
-static int caja_final[Ncol][Nfil]=
+static int caja_final[Nfil][Ncol]=
 {
   {0, 100, 0, 100, 0},
   {100, 0, 100, 0, 100},
@@ -46,7 +51,7 @@ static int caja_final[Ncol][Nfil]=
   {0, 100, 0, 300, 1}
 };
 
-tEstado *crearEstado(int celdas[Ncol][Nfil]);
+tEstado *crearEstado(int celdas[Nfil][Ncol]);
 tEstado *estadoInicial();
 int coste(unsigned op, tEstado *estado);
 void dispEstado(tEstado *estado);
@@ -57,5 +62,4 @@ tEstado *aplicaOperador(unsigned op, tEstado *estado);
 int iguales(tEstado *s, tEstado *t);
 int testObjetivo(tEstado *estado);
 
-int obtener_i(tEstado *estado);
-int obtener_j(tEstado *estado);
+#endif // END OF CASILLAS_H

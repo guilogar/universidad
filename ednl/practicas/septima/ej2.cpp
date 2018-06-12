78a09d2c6fa2c709ce3a429de21c2f669ce5e688
guilogar <jdkdejava@gmail.com>
2018-06-12 18:33:59 +0200

    Ejercicio del laberinto de ednl terminado.

--- ednl/practicas/septima/ej2.cpp
+++ ednl/practicas/septima/ej2.cpp
@@ -1,26 +1,52 @@
 #include <iostream>
 #include "../grafos/grafoPMC.h"
 #include "../grafos/alg_grafoPMC.h"
+#include "../grafos/pilaenla.h"
 
-template <typename tCoste> double laberinto(int n, vector<vector<bool>> paredes,
+template <typename tCoste> double laberinto(int n, Pila<typename GrafoP<tCoste>::arista> paredes,
                                             typename GrafoP<tCoste>::vertice entrada,
-                                            typename GrafoP<tCoste>::vertice salida)
+                                            typename GrafoP<tCoste>::vertice salida,
+                                            typename GrafoP<tCoste>::tCamino& cam)
 {
     GrafoP<tCoste> G(n*n);
     typedef typename GrafoP<tCoste>::vertice vertice;
+    typedef typename GrafoP<tCoste>::arista arista;
     const size_t n_vertices = G.numVert();
     
     for (size_t i = 0; i < n_vertices; i++) {
         for (size_t j = 0; j < n_vertices; j++) {
-            //if(i == j) G[i][j] == GrafoP<tCoste>::INFINITO;
-            if(paredes[i][j]) {
-                G[i][j] = GrafoP<tCoste>::INFINITO;
-                //G[j][i] = GrafoP<tCoste>::INFINITO;
+            if(i == j) G[i][j] = 0;
+            else {
+                if(i >= n && i < ((n-1)*n)) {
+                    G[i][i-n] = 1;
+                    G[i][i+n] = 1;
+                } else if(i < n) {
+                    G[i][i+n] = 1;
+                } else if(i >= ((n-1)*n)) {
+                    G[i][i-n] = 1;
+                }
+                
+                if(i % n > 0 && i % n < (n - 1)) {
+                    G[i][i+1] = 1;
+                    G[i][i-1] = 1;
+                } else if(i % n == 0) {
+                    G[i][i+1] = 1;
+                } else if(i % n == (n - 1)) {
+                    G[i][i-1] = 1;
+                }
             }
         }
     }
-    
-    for (size_t i = 0; i < n_vertices; i++) {
-        
+    while(!paredes.vacia()) {
+        arista a = paredes.tope();
+        paredes.pop();
+        G[a.orig][a.dest] = GrafoP<tCoste>::INFINITO;
     }
+    
+    vector<vertice> dij;
+    vector<tCoste> sol = Dijkstra(G, entrada, dij);
+    
+    cam = camino<tCoste>(entrada, salida, dij);
+    
+    return sol[salida];
 }
--- ednl/practicas/septima/main.cpp
+++ ednl/practicas/septima/main.cpp
@@ -8,6 +8,7 @@
 #include "./ej6.cpp"
 #include "./ej8.cpp"
 #include "./ej11.cpp"
+#include "../grafos/pilaenla.h"
 
 #include <map>
 
@@ -35,49 +36,61 @@ int main(int argc, const char *argv[])
     //std::cout << g << std::endl;
     
     // Ejercicio 2
-    
+    typedef typename GrafoP<int>::arista arista;
+    typedef typename GrafoP<int>::tCamino camino;
+    Pila<arista> paredes;
+    paredes.push(arista(15, 16));
+    camino c;
+    std::cout << "El coste para salir es => " << laberinto<int>(5, paredes, 0, 21, c) << std::endl;
+    std::cout << c << std::endl;
     
     // Ejercicio 3
-    vector<int> cantidadAlmacenadaCiudad(10, 0);
-    almacen<int>(g, {5, 80, 0, 0, 0, 0, 60, 95, 1, 8},
-                 {0, 5, 2, 3, 4, 5, 8, 9, 6, 7},
-                 0, 20, cantidadAlmacenadaCiudad);
+    /*
+     *vector<int> cantidadAlmacenadaCiudad(10, 0);
+     *almacen<int>(g, {5, 80, 0, 0, 0, 0, 60, 95, 1, 8},
+     *             {0, 5, 2, 3, 4, 5, 8, 9, 6, 7},
+     *             0, 20, cantidadAlmacenadaCiudad);
+     */
     
     // Ejercicio 6
-    GrafoP<int> tren(3), autobus(3);
-    
-    tren[0] = {0, 2, GrafoP<int>::INFINITO};
-    tren[1] = {5, 0, 30};
-    tren[2] = {1, 7, 0};
-    
-    autobus[0] = {0, 3, 50};
-    autobus[1] = {3, 0, 14};
-    autobus[2] = {1, 5, 0};
+    /*
+     *GrafoP<int> tren(3), autobus(3);
+     *
+     *tren[0] = {0, 2, GrafoP<int>::INFINITO};
+     *tren[1] = {5, 0, 30};
+     *tren[2] = {1, 7, 0};
+     *
+     *autobus[0] = {0, 3, 50};
+     *autobus[1] = {3, 0, 14};
+     *autobus[2] = {1, 5, 0};
+     */
     //std::cout << agenciaTransportes<int>(tren, autobus, 1) << std::endl;
     
     // Ejercicio 8
-    std::cout << soloUnTransbordo<int>(tren, autobus, 0, 2) << std::endl;
+    //std::cout << soloUnTransbordo<int>(tren, autobus, 0, 2) << std::endl;
     
     // Ejercicio 11
-    GrafoP<int> isla1(3), isla2(2), isla3(3);
-    
-    isla1[0] = {0, 2, 3};
-    isla1[1] = {1, 0, 3};
-    isla1[2] = {1, 2, 0};
-    
-    isla2[0] = {0, 2};
-    isla2[1] = {1, 0};
-    
-    isla3[0] = {0, 2, 3};
-    isla3[1] = {1, 0, 4};
-    isla3[2] = {1, 2, 0};
-    
-    std::map<int, int> mapa;
-    mapa.insert(std::make_pair(7, 5));
-    mapa.insert(std::make_pair(0, 6));
-    
-    vector<std::map<int, int>> puentes(8, std::map<int, int> ());
-    puentes[4] = mapa;
+    /*
+     *GrafoP<int> isla1(3), isla2(2), isla3(3);
+     *
+     *isla1[0] = {0, 2, 3};
+     *isla1[1] = {1, 0, 3};
+     *isla1[2] = {1, 2, 0};
+     *
+     *isla2[0] = {0, 2};
+     *isla2[1] = {1, 0};
+     *
+     *isla3[0] = {0, 2, 3};
+     *isla3[1] = {1, 0, 4};
+     *isla3[2] = {1, 2, 0};
+     *
+     *std::map<int, int> mapa;
+     *mapa.insert(std::make_pair(7, 5));
+     *mapa.insert(std::make_pair(0, 6));
+     *
+     *vector<std::map<int, int>> puentes(8, std::map<int, int> ());
+     *puentes[4] = mapa;
+     */
     
     //std::cout << huries<int>(isla1, isla2, isla3, puentes) << std::endl;
     
